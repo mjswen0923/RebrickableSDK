56ee40af259e5d97307e682ad51530792f438898
mjswen0923 <swenson.m@northeastern.edu>
2021-07-10 21:47:17 -0400

    adds rub capability

--- Sources/RebrickableSDK/APIs/Models/LegoColor.swift
+++ Sources/RebrickableSDK/APIs/Models/LegoColor.swift
@@ -3,11 +3,13 @@ import Foundation
 public struct LegoColor: Codable, Hashable {
     public let id: Int
     public let name: String?
+    public let rgb: String
     public let is_trans: Bool
     
-    public init(id: Int, name: String?, is_trans: Bool) {
+    public init(id: Int, name: String?, rgb: String, is_trans: Bool) {
         self.id = id
         self.name = name
+        self.rgb = rgb
         self.is_trans = is_trans
     }
 }
